b8013c7967757b6c8d2a429c3287bb183676482d
desparvis <latifahm501@gmail.com>
2025-02-14 12:43:48 +0200

    storing the data in the db

---
+++ store_data_in_db.py
@@ -0,0 +1,21 @@
+"""
+importing sqlite3 and csv module
+"""
+import sqlite3
+import csv
+
+# connecting to the database sqlite file
+conn = sqlite3.connect("mydatabase.db")
+cursor = conn.cursor()
+
+# open the csv file and read the cleaned data
+with open('cleaned_data.csv') as csv_file:
+    reader = csv.reader(csv_file)
+    for i in reader:
+        # inserting the data into the table in the database
+        # i is row of the csv file
+        cursor.execute("INSERT INTO momo (address, date, amount, category, transaction_id) VALUES (?, ?, ?, ?, ?)", i)
+# commit changes and close the connection to the database
+conn.commit()
+conn.close
+
